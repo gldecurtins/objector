5eb7e30a9d9ae088c484a28004a6d6030382a77a
Gian Luca Decurtins <gldecurtins@icloud.com>
2022-02-04 15:15:39 +0000

    Remove local file mapping

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Location, Object
+from .models import Location, Object, Sensor
 
-admin.site.register(Object)
 admin.site.register(Location)
+admin.site.register(Object)
+admin.site.register(Sensor)

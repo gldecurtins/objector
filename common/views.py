4b50c86d552b8e5ed82fa7138ff044949bd0cb02
Gian Luca Decurtins <gldecurtins@icloud.com>
2022-05-05 08:51:44 +0000

    Tasks on dashboard

--- common/views.py
+++ common/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth import logout as django_logout
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from inventory.models import Location
 from django.views.generic import RedirectView
+from inventory.models import Location
+from maintenance.models import Task
 
 
 class HomeTemplateView(TemplateView):
@@ -28,10 +29,52 @@ class DashboardTemplateView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         groups = self.request.user.groups.values_list("pk", flat=True)
         groups_as_list = list(groups)
+        overdue_tasks_queryset = (
+            Task.objects.filter(
+                object__owner=self.request.user, status=Task.Statuses.OVERDUE
+            )
+            | Task.objects.filter(
+                object__management_group__in=groups_as_list,
+                status=Task.Statuses.OVERDUE,
+            )
+            | Task.objects.filter(
+                object__maintenance_group__in=groups_as_list,
+                status=Task.Statuses.OVERDUE,
+            )
+        )
+        due_tasks_queryset = (
+            Task.objects.filter(
+                object__owner=self.request.user, status=Task.Statuses.DUE
+            )
+            | Task.objects.filter(
+                object__management_group__in=groups_as_list,
+                status=Task.Statuses.DUE,
+            )
+            | Task.objects.filter(
+                object__maintenance_group__in=groups_as_list,
+                status=Task.Statuses.DUE,
+            )
+        )
+        pending_tasks_queryset = (
+            Task.objects.filter(
+                object__owner=self.request.user, status=Task.Statuses.PENDING
+            )
+            | Task.objects.filter(
+                object__management_group__in=groups_as_list,
+                status=Task.Statuses.PENDING,
+            )
+            | Task.objects.filter(
+                object__maintenance_group__in=groups_as_list,
+                status=Task.Statuses.PENDING,
+            )
+        )
         location_queryset = (
             Location.objects.filter(owner=self.request.user)
             | Location.objects.filter(management_group__in=groups_as_list)
             | Location.objects.filter(maintenance_group__in=groups_as_list)
         )
+        context["overdue_tasks_count"] = overdue_tasks_queryset.count()
+        context["due_tasks_count"] = due_tasks_queryset.count()
+        context["pending_tasks_count"] = pending_tasks_queryset.count()
         context["locations"] = location_queryset
         return context

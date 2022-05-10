d6e0a21f2304b130e6c2769682b5cc21d75cee0b
Gian Luca Decurtins <gldecurtins@icloud.com>
2022-05-10 14:10:15 +0000

    Filter on objects

--- maintenance/filters.py
+++ maintenance/filters.py
@@ -5,6 +5,4 @@ import django_filters
 class TaskFilter(django_filters.FilterSet):
     class Meta:
         model = Task
-        fields = {
-            "status": ["exact"],
-        }
+        fields = ["status", "object"]

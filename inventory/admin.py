257c707d09b338639e98920e9aad808b69a7442f
Gian Luca Decurtins <gldecurtins@icloud.com>
2022-05-13 08:53:15 +0000

    Admin list and edit

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 from .models import Location, Object, Sensor
 
-admin.site.register(Location)
-admin.site.register(Object)
-admin.site.register(Sensor)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "status",
+        "owner",
+        "management_group",
+        "maintenance_group",
+    )
+    list_editable = ("status",)
+
+
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "location",
+        "status",
+        "owner",
+        "management_group",
+        "maintenance_group",
+    )
+    list_editable = ("status",)
+
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "object", "status")
+    list_editable = ("status",)
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Object, ObjectAdmin)
+admin.site.register(Sensor, SensorAdmin)
--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Task, Journal
 
-admin.site.register(Task)
-admin.site.register(Journal)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "object", "status", "due_at", "overdue_at")
+
+
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ("object", "task", "notes", "labor_costs", "material_costs")
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Journal, JournalAdmin)

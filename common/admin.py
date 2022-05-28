52c8ccb9416121f2545e88e2a782fc1ccb86f451
Gian Luca Decurtins <gldecurtins@icloud.com>
2022-05-28 11:20:04 +0000

    add new line

--- common/admin.py
+++ common/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         *UserAdmin.fieldsets,
